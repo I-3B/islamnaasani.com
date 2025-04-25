935237c5984ae0726dd5de2d7cad9183b2ee735e
Islam Naasani <islamnaasani@gmail.com>
2025-04-25 23:32:57 +0300

    feat(rss): add rss feed generation
    - add rss feed generation script using `rss` package which reads blog markdown files and generates `public/rss.xml` during build

--- types/common.ts
+++ types/common.ts
@@ -5,6 +5,7 @@ declare global {
       NEXT_PUBLIC_CONVERTKIT_API_KEY: string;
       NEXT_PUBLIC_CONVERTKIT_FORM_ID: string;
       NEXT_PUBLIC_UMAMI_ID: string;
+      NEXT_PUBLIC_WEBSITE_URL:string
     }
   }
 }
