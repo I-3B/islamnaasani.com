2a51798bf90b3ce67bd2e37d8dc81cbdf749922b
Islam Naasani <islamnaasani@gmail.com>
2025-09-07 14:09:14 +0300

    feat(about): update experience description in About component

--- app/home/About.tsx
+++ app/home/About.tsx
@@ -4,8 +4,8 @@ export const About: FC<AboutProps> = ({}) => {
   return (
     <section className="flex-1 py-3 sm:text-lg">
       <p>
-        I'm a software engineer with 2+ years of experience, a linux enthusiast,
-        and a TypeScript fan.
+        I'm a front-end developer and a software engineer with 3+ years of
+        experience, a linux enthusiast, and a TypeScript fan.
       </p>
       <p className="mt-2">
         Sometimes, I share new learnings or encountered problems.{" "}
